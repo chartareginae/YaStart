c04cf34852ab316e8ab9fe80171abaef0dc3004d
chartareginae <bma4117@gmail.com>
2024-04-22 17:46:56 +0500

    main

--- main.py
+++ main.py
@@ -10,9 +10,15 @@ def main():
     return render_template('main_page.html')
 
 
+@app.route('/registration_and_login')
+def registration_and_login():
+    return render_template('login.html')
+
+
 @app.route('/sphere')
 def sphere():
     return render_template('mars.html', title='OK')
+
 
 if __name__ == '__main__':
     app.run(port=8000, host='127.0.0.1')
